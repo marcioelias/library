3676608829b2a0a797a1621a47b87ac868885bfb
Marcio <marcioelias@gmail.com>
2021-07-26 18:32:20 -0300

    Added redirects and destroy method

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -9,10 +9,18 @@ class BooksController extends Controller
 {
     public function store(Request $request) {
         Book::create($this->validateRequest($request));
+        return redirect('/books');
     }
 
     public function update(Request $request, Book $book) {
         $book->update($this->validateRequest($request));
+        return redirect('/books');
+    }
+
+    public function destroy(Book $book) {
+        $book->delete();
+
+        return redirect('/books');
     }
 
     protected function validateRequest(Request $request) {
